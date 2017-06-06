52e668cc6927f6d959c61b3e582c9a8634d9a20b
JoMingyu <city7310@naver.com>
2017-06-06 22:50:06 +0900

    writeHead

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ express = require('express');
 app = express();
 
 app.get('/', (req, res) => {
+	res.writeHead(200, {'Content-Type' : 'text/plain', 'Test' : 10});
+	// Object(key-value) type
+	
 	res.end('hello');
 });
 
