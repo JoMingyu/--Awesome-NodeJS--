baa9e1ac279d109079c5facb6de8b46cfde213e6
JoMingyu <city7310@naver.com>
2017-06-07 16:20:22 +0900

    connect MongoDB

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-var express = require('express');
-var app = express();
+var express		= require('express');
+var app				= express();
+var mongoose	= require('mongoose');
+var db					= mongoose.connection;
 
-app.use('/account', require('./account'))
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', () => {
+	console.log("Connected");
+});
+mongoose.connect('mongodb://localhost:27017/test');
+
+app.use('/account', require('./rest_modules/account'))
 
 app.listen(80, () => {
 	console.log('Server is listening');
