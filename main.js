a9e277a9c0c5fd0f7e653323d0aeb652c9438744
JoMingyu <city7310@naver.com>
2017-06-07 17:24:40 +0900

    signup & signin

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
-var express		= require('express');
-var app				= express();
-var mongoose	= require('mongoose');
-var db					= mongoose.connection;
+var express = require('express');
+var app = express();
 
-db.on('error', console.error.bind(console, 'connection error: '));
-db.once('open', () => {
-	console.log("Connected");
-});
-mongoose.connect('mongodb://localhost:27017/test');
-
-app.use('/account', require('./rest_modules/account'))
+app.use('/', require('./rest_modules/account'));
 
 app.listen(80, () => {
 	console.log('Server is listening');
--- rest_modules/account.js
+++ rest_modules/account.js
@@ -1,15 +1,47 @@
 var express = require('express');
 var router = express.Router();
+module.exports = router;
 
 var multer = require('multer');
 var form = multer();
 
-module.exports = router;
+var mongoose = require('mongoose');
+var db	 = mongoose.connection;
+mongoose.connect('mongodb://localhost:27017/test');
 
-router.get('/', (req, res) => {
-	console.log(req.query.test);
+var accountSchema = mongoose.Schema({
+	id: String,
+	pw: String
 });
 
-router.post('/', form.fields([]) , (req, res) => {
-	console.log(req.body);
+var accountModel = mongoose.model("account", accountSchema);
+new accountModel().save();
+
+router.post('/signin', form.fields([]), (req, res) => {
+	var id = req.body.id;
+	var pw = req.body.pw;
+	
+	accountModel.find({id: id, pw: pw}, (err, docs) => {
+		if(docs.length) {
+			res.sendStatus(201);
+		} else {
+			res.sendStatus(204);
+		}
+	});
+});
+
+router.post('/signup', form.fields([]) , (req, res) => {
+	var id = req.body.id;
+	var pw = req.body.pw;
+	
+	accountModel.find({id: id}, (err, docs) => {
+		if(docs.length) {
+			res.sendStatus(204);
+		} else {
+			var ins = new accountModel({id: id, pw: pw});
+			ins.save();
+			
+			res.sendStatus(201);
+		}
+	});
 });
