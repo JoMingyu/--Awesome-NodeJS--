7ce68d9eab838bb4cdeef5631209938d36a2226c
JoMingyu <city7310@naver.com>
2017-06-07 19:20:38 +0900

    cookie

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+var cookie = module.exports = require('cookie-parser');
 
+app.use(cookie('!wo.2#ksd)@'));
 app.use('/', require('./rest_modules/account'));
+app.use('/', require('./rest_modules/post'));
 
 app.listen(80, () => {
 	console.log('Server is listening');
--- rest_modules/account.js
+++ rest_modules/account.js
@@ -4,6 +4,7 @@ module.exports = router;
 
 var multer = require('multer');
 var form = multer();
+var cookie = require('../app');
 
 var mongoose = require('mongoose');
 var db	 = mongoose.connection;
@@ -11,7 +12,8 @@ mongoose.connect('mongodb://localhost:27017/test');
 
 var accountSchema = mongoose.Schema({
 	id: String,
-	pw: String
+	pw: String,
+	session_id: String
 });
 
 var accountModel = mongoose.model("account", accountSchema);
@@ -23,6 +25,9 @@ router.post('/signin', form.fields([]), (req, res) => {
 	
 	accountModel.find({id: id, pw: pw}, (err, docs) => {
 		if(docs.length) {
+			var sessionId = guid();
+			accountModel.update({id: id}, {$set:{session_id: sessionId}});
+			res.cookie('cookie', sessionId, {signed: true});
 			res.sendStatus(201);
 		} else {
 			res.sendStatus(204);
@@ -38,10 +43,19 @@ router.post('/signup', form.fields([]) , (req, res) => {
 		if(docs.length) {
 			res.sendStatus(204);
 		} else {
-			var ins = new accountModel({id: id, pw: pw});
-			ins.save();
+			var instance = new accountModel({id: id, pw: pw});
+			instance.save();
 			
 			res.sendStatus(201);
 		}
 	});
 });
+
+function guid() {
+	function s4() {
+		return Math.floor((1 + Math.random()) * 0x10000)
+		.toString(16)
+		.substring(1);
+	}
+	return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
