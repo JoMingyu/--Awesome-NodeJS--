029571f04da198800d6955e87c0fc61118a8cdf6
JoMingyu <city7310@naver.com>
2017-06-29 13:19:11 +0900

    Arguments

--- 011. Arguments/app.js
+++ 011. Arguments/app.js
@@ -1,6 +1,6 @@
 function sum() {
-  // 인자에 대한 정의가 없지만 마지막 라인에서는 3개의 인자를 전달하고 있음
-  // 함수 안에서 사용할 수 있도록 그 이름이나 특성이 약속되어 있는 일종의 배열
+  // 인자에 대한 정의가 없지만 21라인에서는 3개의 인자를 전달하고 있음
+  // arguments는 함수 안에서 사용할 수 있도록 약속되어 있는 일종의 배열
   // 실제로는 arguments 객체의 인스턴스
   var sum = 0;
   for(var i = 0; i < arguments.length; i++) {
@@ -14,6 +14,7 @@ function lengthExample(a, b) {
   return {
     'func_length' : lengthExample.length,
     'args_length' : arguments.length
+    // 함수에 대한 length는 매개변수의 갯수를 의미함
   };
 }
 
