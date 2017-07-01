3cf94d4de1de48bdbdd843076ef46e92743e374d
JoMingyu <city7310@naver.com>
2017-07-01 12:39:32 +0900

    ES6 - Template Literals, Substitution

--- 021-2. ES6 - Template Literal, Substitution/app.js
+++ 021-2. ES6 - Template Literal, Substitution/app.js
@@ -1,3 +1,8 @@
 let s = `This is template literals. We can use ' and " in here.`;
-console.log(`Content = ${s}`); // Template Substitution
+// Template literals
+// 백틱(`) 문자를 사용한다
+
+console.log(`Content = ${s}`);
+// Template Substitution
+
 console.log(`Upper = ${s.toUpperCase()}`);
