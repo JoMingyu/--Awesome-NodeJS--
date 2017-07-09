ad4b36b8a606e5747868221996867f92a5966d4e
JoMingyu <city7310@naver.com>
2017-07-09 21:31:08 +0900

    JSON

--- 023. JSON/app.js
+++ 023. JSON/app.js
@@ -3,7 +3,7 @@ obj.key3 = 3;
 
 let arr = ['value1', 'value2', 'value3'];
 arr.push('value4');
-console.log(arr);
+console.log(JSON.stringify(arr));
 
 let text = '{"key1" : 1, "key2" : 2}';
 // key는 큰따옴표로 묶여 있는 형태여야 파싱이 가능
