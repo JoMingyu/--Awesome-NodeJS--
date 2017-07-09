69c2a862393d92ce9adc0199d702ff115742b998
PlanB <city7310@naver.com>
2017-07-09 14:22:28 +0900

    JSON

---
+++ 023. JSON/app.js
@@ -0,0 +1,18 @@
+let obj = {key1 : 1, key2 : 2};
+obj.key3 = 3;
+
+let arr = ['value1', 'value2', 'value3'];
+arr.push('value4');
+console.log(arr);
+
+let text = '{"key1" : 1, "key2" : 2}';
+// key는 큰따옴표로 묶여 있는 형태여야 파싱이 가능
+
+let parsedObj = JSON.parse(text);
+// text to JSON
+
+parsedObj.key3 = 3;
+text = JSON.stringify(parsedObj);
+// JSON to text
+
+console.log(text);
