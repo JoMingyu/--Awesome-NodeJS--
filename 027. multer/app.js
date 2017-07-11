6bb642f6709c30c2b76964e9f9d10aef9ef6bf41
JoMingyu <city7310@naver.com>
2017-07-11 10:49:39 +0900

    multer

---
+++ 027. multer/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const form = require('multer');
+
+app.post('/index', form.fields([]), (req, res) => {
+    res.sendStatus(201);
+});
+
+app.listen(80, () => {
+    console.log('Server is listening');
+});
