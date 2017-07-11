39015a058f80263f5ee7830ffb3577c92b61c299
JoMingyu <city7310@naver.com>
2017-07-11 12:58:52 +0900

    multer

--- 027. multer/app.js
+++ 027. multer/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
-const form = require('multer');
+const form = require('multer')();
 
 app.post('/index', form.fields([]), (req, res) => {
+    console.log(req.body.key);
     res.sendStatus(201);
 });
 
