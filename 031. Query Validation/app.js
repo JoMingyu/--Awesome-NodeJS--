5dd1c86a3f1163f8a0da7c9a7f665996a2005216
JoMingyu <city7310@naver.com>
2017-07-12 10:49:07 +0900

    Query Validation

---
+++ 031. Query Validation/app.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host : 'localhost',
+    port : 3306,
+    database : 'test_db',
+    user : 'root',
+    password : 'uursty199'
+});
+
+connection.connect();
+
+connection.query(`SELECT * FROM test_table WHERE col1='a'`, (err, rows) => {
+    if(rows != undefined) {
+        console.log(rows);
+    } else {
+        console.log(undefined);
+    }
+});
+
+connection.query(`INSERT INTO test_table VALUES('asdf', 'qwer')`, (err, rows) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('success');
+    }
+});
