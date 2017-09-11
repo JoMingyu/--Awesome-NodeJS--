61a8d092d6444ac5735a15f9506981ca50e71442
PlanB <city7310@naver.com>
2017-09-12 00:03:12 +0900

    mongoose

--- 037. mongoose/app.js
+++ 037. mongoose/app.js
@@ -27,7 +27,7 @@ const Post = mongoose.model('Post', postSchema);
 const post = new Post({
     title: 'hello',
     content: 'Im so tired',
-    authro: 'PlanB'
+    author: 'PlanB'
 });
 
 // CRUD 중 C
