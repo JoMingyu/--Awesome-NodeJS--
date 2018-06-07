9265e1d618af49434a2645d2084c21e47cd84c19
JoMingyu <city7310@naver.com>
2018-06-07 09:23:15 +0900

    04

---
+++ 04. Type - String & Template Literal/index.js
@@ -0,0 +1,8 @@
+var s = 'PlanB';
+// 문자열은 작은따옴표 또는 큰따옴표로 감싸며, String 타입으로 wrapping된다
+
+var n = 3
+
+console.log(`Name is ${n}`)
+// `... ${expression} ...` 형태로 템플릿 리터럴을 만든다
+// quote나 double quote가 아닌 backqoute를 사용한다는 것에 주의해야 한다
