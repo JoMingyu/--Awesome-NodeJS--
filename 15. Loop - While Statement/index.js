83195cf10eddcedc0045a7c42806f82c3ca7703c
JoMingyu <city7310@naver.com>
2018-06-10 22:14:13 +0900

    15

---
+++ 15. Loop - While Statement/index.js
@@ -0,0 +1,13 @@
+// while문은 타 언어처럼 while (condition) { ... } 형태로 사용한다
+var i = 0;
+
+while(i < 10) {
+    console.log(i);
+
+    i++;
+}
+
+// do-while 가능
+do {
+    console.log('Hi?');
+} while(false);
