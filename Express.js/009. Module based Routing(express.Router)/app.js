9c6c5cef382068c43961ca792f8e4d8043866393
JoMingyu <city7310@naver.com>
2018-06-12 08:39:13 +0900

    Express 부분 제거

--- Express.js/009. Module based Routing(express.Router)/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use('/router', require('./module'));
-// 모듈의 라우팅 핸들러들은 /router 하위에 적용됨
-
-app.listen(80, () => {
-
-});
--- Express.js/011. UUID/app.js
+++
@@ -1,7 +0,0 @@
-const uuid = require('node-uuid');
-// npm install node-uuid
-
-while(true) {
-    console.log(uuid.v4());
-    // v1과 v4 uuid를 불러올 수 있음
-}
